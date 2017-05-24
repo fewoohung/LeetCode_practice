9cfec3663bd1b502ab1a43a50082a54270fccfb7
fewoohung <fewoohung@gmail.com>
2017-05-24 17:18:02 +0800

    Update quit_007.cpp

--- quit_007.cpp
+++ quit_007.cpp
@@ -3,16 +3,16 @@ public:
     int reverse(int x) {
         
         int value=0;
-		int temp=0;
-		int check=0;
+	int temp=0;
+	int check=0;
         while(x!=0)
         {
-			temp = value;
+	    temp = value;
             value = x%10 + value*10;
             x = x/10;
-			check = value/10;            
+	    check = value/10;            
             
-			if(check != temp)
+	    if(check != temp)
             {
                 return 0;
             }
